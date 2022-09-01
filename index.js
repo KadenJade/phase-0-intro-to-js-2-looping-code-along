a15362369c29d7cab073e4aecf75a98347c16605
Kaden <kadengraybill@gmail.com>
2022-09-01 08:49:48 -0500

    commit tree 05e76ce2007b52e096ca17c525012d21bf3b7e11

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const newCards = []
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    newCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+ 
+}
+return newCards
+}
+//countDown
+function countDown(int){
+    while (int > -1){
+        console.log(int--)
+    }
+}
+countDown()
+
